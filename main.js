188ccdf5c5611d2ce3ddeb8080c9c9c39f747806
Mohammad Abuzahid <mohammadhijjawi7@gmail.com>
2021-10-16 12:07:41 -0700

    inital commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function myFunction(){
+
+    let x = document.body.style.backgroundColor="red";
+    
+    var img = document.createElement("img");
+    img.src = "mohammad.jpg";
+    var src = document.getElementById("x");
+    src.appendChild(img);
+
+}
